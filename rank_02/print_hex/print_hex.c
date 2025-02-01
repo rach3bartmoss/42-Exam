d1a37185671bb5589b70d23855e2342e94449d87
Douglas Alves <rachebartm00s@proton.me>
2025-02-01 23:26:18 +0000

    h

--- rank_02/print_hex/print_hex.c
+++ rank_02/print_hex/print_hex.c
@@ -3,61 +3,57 @@
 #include <stdlib.h>
 #include <string.h>
 
+int	ft_strlen(char *str)
+{
+	int	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
 int	ft_atoi(char *str)
 {
-	char	*base = "0123456789";
-	int	i = strlen(str) - 1;
 	int	digit = 1;
-	int	result = 0;
+	int	res = 0;
+	int	i = ft_strlen(str) - 1;
 
 	while (i >= 0)
 	{
-		result += ((str[i] - '0') * digit);
+		res += ((str[i] - '0') * digit);
 		digit = digit * 10;
 		i--;
 	}
-	return (result);
+	return (res);
 }
 
-void	print_hex(int n)
+void	print_hex(int	n)
 {
 	char	*base = "0123456789abcdef";
-	char	remainder;
-	char	*to_print = malloc(8 * sizeof(char));
-	int		result;
-	int	i = 0;
+	char	buffer[9];
+	int		i = 0;
 
-	while (n > 0)
+	if (n == 0)
 	{
-		result = n / 16;
-		remainder = n % 16;
-		n = result;
-		if (remainder <= 9)
-		{
-			to_print[i] = base[remainder];
-		}
-		else
-		{
-			to_print[i] = base[remainder];
-		}
-		i++;
+		write(1, "0", 1);
 	}
-	i--;
-	while (i >= 0)
+	while (n != 0)
 	{
-		write (1, &to_print[i], 1);
-		i--;
+		buffer[i++] = base[n % 16];
+		n /= 16;
 	}
-	write (1, "\n", 1);
+	while (i > 0)
+		write(1, &buffer[--i], 1);
+	write(1, "\n", 1);
 }
 
 int	main(int ac, char **av)
 {
 	if (ac != 2)
 	{
-		write (1, "\n", 1);
+		write(1, "\n", 1);
 		return (0);
 	}
 	int	n = ft_atoi(av[1]);
+	//printf("ft_atoi:%d\n", n);
 	print_hex(n);
 }
