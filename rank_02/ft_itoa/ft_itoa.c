ca39dd69a94296a7388263de309656ca65f786a3
Douglas Alves <rachebartm00s@proton.me>
2025-02-03 11:32:42 +0000

    itoa

--- rank_02/ft_itoa/ft_itoa.c
+++ rank_02/ft_itoa/ft_itoa.c
@@ -1,58 +1,81 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
-int	count_digit(int n)
+int	count_digits(int n)
 {
 	int	count = 0;
+	if (n == -2147483648)
+		return (10);
 	if (n < 0)
 		n *= -1;
-
 	while (n > 0)
 	{
-		n = n / 10;
+		n /= 10;
 		count++;
 	}
 	return (count);
 }
 
-char	*ft_itoa(int nbr)
+void	ft_strcpy(char *dest, char *src)
 {
-	int	sign = 0;
-	int	indexes = 0;
+	int	i = 0;
 
-	if (0 > nbr)
+	while (src[i] != '\0')
 	{
-		nbr *= -1;
-		sign = 1;
-		indexes = count_digit(nbr) + 1;
+		dest[i] = src[i];
+		i++;
 	}
-	else
-		indexes = count_digit(nbr);
-	char	*result = malloc(indexes * sizeof(char));
-	if (nbr == 0)
-	{
-		result[0] = '0';
-		result[1] = '\0';
-		return (result);
-	}
-	int	save = nbr;
-
-	indexes--;
-	while (indexes >= 0)
-	{
-		result[indexes] = (nbr % 10) + '0';
-		nbr = nbr / 10;
-		if (indexes == 0 && sign == 1)
-			result[indexes] = '-';
-		indexes--;
-	}
-	result[save] = '\0';
-	return (result);
+	dest[i] = '\0';
 }
 
-/*int	main(void)
+char	*ft_itoa(int nbr)
 {
-	char	*str = ft_itoa(156);
-	printf("%s\n", str);
-}*/
+	char	*str;
+	int		digits;
+	int		neg;
+
+	str = malloc(sizeof(char) * 12);
+	digits = count_digits(nbr) - 1;
+	neg = 0;
+	char	*base = "0123456789";
+	if (nbr == 0)
+	{
+		ft_strcpy(str, "0");
+		return (str);
+	}
+	if (nbr == -2147483648)
+	{
+		ft_strcpy(str, "-2147483648");
+		return (str);
+	}
+	if (nbr < 0)
+	{
+		neg = 1;
+		nbr *= -1;
+		digits++;
+	}
+	while (nbr > 0)
+	{
+		str[digits] = base[nbr % 10];
+		nbr /= 10;
+		digits--;
+	}
+	if (neg)
+	{
+		str[digits] = '-';
+	}
+	return (str);
+}
+
+/*int	main(int ac, char **av)
+{
+	if (ac != 2)
+		return (write(1, "\n", 1));
+
+	int	n = atoi(av[1]);
+	char	*res = ft_itoa(n);
+
+	printf("%s\n", res);
+	free(res);
+	}*/
