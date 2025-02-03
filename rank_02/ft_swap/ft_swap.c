3f0f1a60d818bbc2ec6f406f0683086c3952a6a8
Douglas Alves <rachebartm00s@proton.me>
2025-02-03 11:33:06 +0000

    swap

--- rank_02/ft_swap/ft_swap.c
+++ rank_02/ft_swap/ft_swap.c
@@ -1,21 +1,19 @@
-#include <unistd.h>
 #include <stdio.h>
 
 void	ft_swap(int *a, int *b)
 {
-	int	temp = *a;
+	int	temp = 0;
+
+	temp = *a;
 	*a = *b;
 	*b = temp;
 }
 
 /*int	main(void)
 {
-	int	a = 5;
-
-	int	b = 20;
+	int	a = 10;
+	int	b = 80;
 
 	ft_swap(&a, &b);
-
-	printf("a:%d\n", a);
-
-}*/
+	printf("A:%d\nB:%d\n", a, b);
+	}*/
