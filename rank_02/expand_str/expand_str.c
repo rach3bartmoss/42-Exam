aa2c5cbe8fa13250b9f9a59ca54cbd50a8aa6a9a
Douglas Alves <rachebartm00s@proton.me>
2025-02-05 10:41:14 +0000

    simplify logic for expand str

--- rank_02/expand_str/expand_str.c
+++ rank_02/expand_str/expand_str.c
@@ -1,66 +1,39 @@
 #include <unistd.h>
-#include <stdio.h>
-#include <ctype.h>
-
-int	check_if_null_is_next(char *str, int i)
-{
-	while (str[i])
-	{
-		if (isalpha(str[i]) || str[i] == '.')
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
-int	next_word_index(char *str, int i)
-{
-	while ((str[i] == ' ' || str[i] == '\t') && str[i])
-	{
-		i++;
-	}
-	return (i);
-}
 
 void	expand_str(char *str)
 {
 	int	i = 0;
-	int	check = 0;
-	int	blank_counter = 3;
+	int	space_count = 3;
 
-	while (str[i] != 0)
+	while (str[i] == ' ')
+		i++;
+	while(str[i])
 	{
-		blank_counter = 3;
-		if (str[check] == ' ' && check == 0)
+		space_count = 3;
+		if (str[i] != ' ' && str[i+1] == ' ')
 		{
-			while (str[i] == ' ')
+			write(1, &str[i], 1);
+			i++;
+			while (str[i] == ' ' && str[i] != '\0')
 				i++;
-			check = 1;
-		}
-		if (str[i] == ' ' || str[i] == '\t')
-		{
-			if (check_if_null_is_next(str, i) == 1)
-				blank_counter = 0;
-			while (blank_counter > 0)
+			while (space_count > 0 && str[i] != '\0')
 			{
 				write(1, " ", 1);
-				blank_counter--;
+				space_count--;
 			}
-			i = next_word_index(str, i) - 1;
 		}
-		else
+		else if (str[i] != '\0')
+		{
 			write(1, &str[i], 1);
-		i++;
+			i++;
+		}
 	}
-	write (1, "\n", 1);
+	write(1, "\n", 1);
 }
 
 int	main(int ac, char **av)
 {
 	if (ac != 2)
-	{
 		return (write(1, "\n", 1));
-	}
-	char	*str = av[1];
-	expand_str(str);
+	expand_str(av[1]);
 }
