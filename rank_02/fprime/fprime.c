060438151d17bd02396b9483ced416be6b437750
Douglas Alves <rachebartm00s@proton.me>
2025-02-05 00:12:46 +0000

    fprime

--- rank_02/fprime/fprime.c
+++ rank_02/fprime/fprime.c
@@ -2,43 +2,69 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-void	fprime(unsigned int nb)
+int	ft_strlen(char *str)
 {
-	unsigned int	prime;
+	int	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
 
-	if (nb == 1)
-		printf("1");
-	else
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	res;
+	int	digit;
+
+	i = ft_strlen(str) - 1 ;
+	res = 0;
+	digit = 1;
+
+	while(i >= 0)
 	{
-		prime = 2;
-		while (nb > 1)
-		{
-			if (nb % prime == 0)
-			{
-				printf("%d", prime);
-				nb /= prime;
-				if (nb > 1)
-					printf("*");
-				prime--;
-			}
-			prime++;
-		}
+		res += (str[i] - '0') * digit;
+		digit *= 10;
+		i--;
 	}
+	return (res);
+}
+
+void	fprime(int n)
+{
+
+	if (n == 1)
+	{
+		printf("1\n");
+		return ;
+	}
+	if (n < 0)
+	{
+		printf("\n");
+		return ;
+	}
+	while (n % 2 == 0)
+	{
+		printf("2*");
+		n = n / 2;
+	}
+	int	 i = 3;
+	while(i * i <= n)
+	{
+		while (n % i == 0)
+		{
+			printf("%d*", i);
+			n = n / i;
+		}
+		i += 2;
+	}
+	if (n > 2)
+		printf("%d", n);
+	printf("\n");
 }
 
 int	main(int ac, char **av)
 {
 	if (ac != 2)
-	{
-		printf("\n");
-		return (0);
-	}
-	int	value = atoi(av[1]);
-	if (0 > value)
-	{
-		printf("\n");
-		return (0);
-	}
-	fprime(value);
-	printf("\n");
+		return (write(1, "\n", 1));
+	fprime(atoi(av[1]));
 }
