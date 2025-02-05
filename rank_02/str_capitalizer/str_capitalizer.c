a4f46c369bcf41ee771a67eb851101d5918733e0
Douglas Alves <rachebartm00s@proton.me>
2025-02-05 00:13:27 +0000

    redo

--- rank_02/str_capitalizer/str_capitalizer.c
+++ rank_02/str_capitalizer/str_capitalizer.c
@@ -1,34 +1,60 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <unistd.h>
+
+int	ft_isalpha(char c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	return (0);
+}
 
 void	str_capitalizer(char *str)
 {
 	int	i = 0;
 
-	if (str[i] >= 'a' && str[i] <= 'z')
-		str[i] -= 32;
-	write(1, &str[i], 1);
-	while (str[++i])
+	while (str[i] != '\0')
 	{
-		if (str[i] >= 'A' && 'Z' >= str[i])
-			str[i] += 32;
-		if ((str[i] >= 'a' && 'z' >= str[i]) && (str[i - 1] == ' ' || str[i] == '\t'))
-			str[i] -= 32;
-		write(1, &str[i], 1);
+		if (ft_isalpha(str[0]) && i == 0)
+		{
+			str[i] = toupper(str[i]);
+			write(1, &str[i], 1);
+			i++;
+		}
+		else if (ft_isalpha(str[i]) && (i == 0 || str[i-1] == ' ') && (str[i+1] == ' ' || str[i+1] == '\0'))
+		{
+			str[i] = toupper(str[i]);
+			write(1, &str[i], 1);
+			i++;
+		}
+		else if (ft_isalpha(str[i]) && str[i-1] == ' ' && ft_isalpha(str[i+1]))
+		{
+			str[i] = toupper(str[i]);
+			write(1, &str[i], 1);
+			i++;
+		}
+		else
+		{
+			if (ft_isalpha(str[i]))
+			{
+				str[i] = tolower(str[i]);
+			}
+			write(1, &str[i], 1);
+			i++;
+		}
 	}
+	write(1, "\n", 1);
 }
 
 int	main(int ac, char **av)
 {
-	int	i = 1;
 	if (ac == 1)
-	{
 		return (write(1, "\n", 1));
-	}
+	int	i = 1;
+
 	while (i < ac)
 	{
 		str_capitalizer(av[i]);
-		write(1, "\n", 1);
 		i++;
 	}
-	return (0);
 }
